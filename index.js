02e2d28291087eb11d047e7c2d4f3a4e91ee3a4b
Khoa Phan <khoaphan89@gmail.com>
2019-05-25 16:40:33 -0400

    Set up NodeJS boilerplate

---
+++ index.js
@@ -0,0 +1,16 @@
+// NodeJS boilerplate
+// https://nodejs.org/en/docs/guides/getting-started-guide/
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Goliath online.');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
