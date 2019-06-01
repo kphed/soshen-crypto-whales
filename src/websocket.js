407208e75418b92c0eca6e9de236d58982d7782c
Khoa Phan <khoaphan89@gmail.com>
2019-06-01 15:45:50 -0400

    Wrap ws logic in a function to enable reconnection attempts

--- src/websocket.js
+++ src/websocket.js
@@ -4,39 +4,72 @@
 const WebSocket = require('ws');
 const { tweet } = require('./twitter');
 const { convertTransactionOutputsAmountToAda } = require('./util');
+const { getLatestMarketQuoteForSymbol } = require('./coinmarketcap');
 const {
   whaleTransactionMinimum,
   seizaExplorerTransactionUrl,
+  coinMarketCap,
 } = require('./config');
 
-// Open up a secure WebSocket connection to the soshen node endpoint
-const ws = new WebSocket(`wss://${process.env.SOSHEN_NODE_ENDPOINT}`);
-
-// Since the point of this app is to tweet whenever there's a large transaction, we'd want to subscribe to
-// the channel that provides us with a stream of network transactions as they occur
 const messageChannels = ['transactionCreated'];
 
-// Upon the WebSocket connection being opened, send the server the channels that we would like messages for
-ws.on('open', () => messageChannels.forEach(messageChannel => ws.send(`sub ${messageChannel}`)));
+const connectToWs = () => {
+  // Open up a secure WebSocket connection to the soshen node endpoint
+  const ws = new WebSocket(`wss://${process.env.SOSHEN_NODE_ENDPOINT}`);
 
-ws.on('message', (message) => {
-  // Parse stringified message payload
-  const {
-    event,
-    data,
-  } = JSON.parse(message);
+  // Since the point of this app is to tweet whenever there's a large transaction, we'd want to subscribe to
+  // the channel that provides us with a stream of network transactions as they occur
 
-  if (event === 'transactionCreated') {
-    const transactionAmount = convertTransactionOutputsAmountToAda(data.outputs_amount);
+  // Upon the WebSocket connection being opened, send the server the channels that we would like messages for
+  ws.on('open', () => messageChannels.forEach(messageChannel => ws.send(`sub ${messageChannel}`)));
 
-    // Tweet transaction only if it's greater than or equal to the ADA whale transaction minimum
-    if (transactionAmount >= whaleTransactionMinimum.ada) {
-      const formattedTransactionAmount = transactionAmount.toLocaleString(undefined, { maximumFractionDigits: 2 });
-      const transactionAmountString = `🐳 ${formattedTransactionAmount} #ADA #CARDANO`;
-      const transactionExplorerString = `Transaction: ${seizaExplorerTransactionUrl}/${data.hash}`;
+  // Attempt to reconnect on disconnect every 10 seconds
+  ws.on('close', () => setTimeout(connectToWs, 10000));
 
-      // Tweet transaction amount with link to Seiza Explorer to enable transaction sleuthing
-      return tweet(`${transactionAmountString}\n\n${transactionExplorerString}`)
+  ws.on('message', async (message) => {
+    // Parse stringified message payload
+    const {
+      event,
+      data,
+    } = JSON.parse(message);
+
+    if (event === 'transactionCreated') {
+      const transactionAmount = convertTransactionOutputsAmountToAda(data.outputs_amount);
+
+      // Tweet transaction only if it's greater than or equal to the ADA whale transaction minimum
+      if (transactionAmount >= whaleTransactionMinimum.ada) {
+        try {
+          const symbols = {
+            crypto: 'ADA',
+            fiat: 'USD',
+          };
+
+          // Set nested response `data` property to `responseData` since `data` is already defined
+          const { data: { data: responseData } } = await getLatestMarketQuoteForSymbol(symbols.crypto);
+
+          const cryptocurrency = {
+            name: responseData[symbols.crypto].name,
+            price: responseData[symbols.crypto].quote[symbols.fiat].price,
+            amount: transactionAmount.toLocaleString(undefined, { maximumFractionDigits: 2 }),
+          };
+          const fiat = (cryptocurrency.price * transactionAmount).toLocaleString('en', {
+            style: 'currency',
+            currency: symbols.fiat,
+          });
+          const tweet = {
+            crypto: `🐳 ${cryptocurrency.amount} #${symbols.crypto} #${cryptocurrency.name.toUpperCase()}`,
+            fiat: `💵 ${fiat} #${symbols.fiat} ${coinMarketCap.urls.currencies}/${cryptocurrency.name.toLowerCase()}`,
+            explorer: `🔎 ${seizaExplorerTransactionUrl}/${data.hash}`,
+          };
+
+          // Tweet transaction amount with link to Seiza Explorer to enable transaction sleuthing
+          return tweet(`${tweet.crypto}\n${tweet.fiat}\n${tweet.explorer}`)
+        } catch (err) {
+          throw err;
+        }
+      }
     }
-  }
-});
+  });
+}
+
+connectToWs();
